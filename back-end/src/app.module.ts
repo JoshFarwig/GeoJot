63d3b039438eede25792bde8f11990fbaf6ad3f5
JoshFarwig <54459705+JoshFarwig@users.noreply.github.com>
2024-09-20 16:52:08 -1000

    chore(config,deps): docker, env, prisma inits and organization
    Added some setup for nestjs for prisma service / module and config module. Need to primarily focus
    on the back-end deployment, then can  start to look towards reverse proxy. Still need to set up some
    dockerfiles and other packagejson scripts

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { UsersModule } from './users/users.module';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `./envs/.env${process.env.NODE_ENV || 'development'}`,
+    }),
+    PrismaModule,
+    UsersModule,
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
